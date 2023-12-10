83818b1436778f08dad2f5ec0186ec6f0f392d70
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-12-10 02:04:24 -0600

    update

--- index.js
+++ index.js
@@ -15,27 +15,130 @@ const io = new Server(server, {
     }
 })
 
+const online = [];
+
+const rooms = [];
+
 //runs on conecction
 io.on("connection", (socket) => {
-    console.log('user connected:', socket.id);
+    console.log('User Connected:', socket.id);
+
+    //push user to online
+    online.push({id: socket.id, name: ''});
+
+    console.log('Online Users', online);
 
     socket.on("disconnect", () => {
-        console.log('disconnected', socket.id);
+        console.log('User Disconnected', socket.id);
+
+        //remove user from online users
+        for(let i = 0; i < online.length; i++){
+            if(online[i].id === socket.id){
+                online.splice(i, 1);
+                
+            }
+        }
+
+        console.log('Online Users:', online);
     })
-    //join a room
+
+    //client creates room
+    socket.on("create_room", (data) => {
+        console.log('Create Room:', data);
+
+        //join room
+        socket.join(data.id);
+
+        //get player from online[]
+        let player;
+        for(let i = 0; i < online.length; i++){
+            if(online[i].id === data.id){
+                player = online[i];
+            }
+        }
+
+        const room = { id: data.id, players: [player], chat: [], dealer: [], scores: [] };
+
+        //update room with player in it
+        io.in(data.id).emit("update_room", room)
+
+        //push room into rooms[]
+        rooms.push(room);
+
+        console.log('Rooms:', rooms)
+
+        //update stage
+        io.to(data.id).emit("stage_update", {stage: 'await'});
+
+        //update notifcation
+        io.to(data.id).emit("notification", {message: 'Room created.'});
+    })
+
+    //client join a room
     socket.on("join_room", (data) => {
-        socket.join(data);
+        console.log('Join Room:', data);
+
+        //join room
+        socket.join(data.room);
+
+        //get player info from online[]
+        let player;
+        for(let i = 0; i < online.length; i++){
+            if(online[i].id === data.id){
+               player = online[i];
+            }
+        }
+
+        //find room
+        let room;
+        for(let i = 0; i < rooms.length; i++){
+            if(rooms[i].id === data.room){
+                room = rooms[i];
+            }
+        }
+
+        //push player into room           
+        room.players.push(player);
+
+        //update room
+        io.in(room.id).emit("update_room", room);
+        
+        //update chat
+        io.to(data.room).emit("receive_message", room.chat);
+
+        //update stage
+        io.to(data.id).emit("stage_update", {stage: 'await'});
+
+        //update notifcation
+        io.to(data.id).emit("notification", {message: 'Joined room.'});
     })
 
+    //update user name
+    socket.on("update_name", (data) => {
+        for(let i = 0; i < online.length; i++){
+            if(online[i].id === data.id){
+                online[i].name = data.name;
+                io.to(data.id).emit("notification", {message: 'Name updated.'})
+            }
+        }
+        console.log('online users:', online);
+    })
+
+    //send message
     socket.on("send_message", (data) => {
-        console.log("data", data);
+        console.log('Send Message:', data)
 
-        //send to everyone but yourself
-        // socket.broadcast.emit("recieve_message", data);
-
-        //send to specific room
-        socket.to(data.room).emit("recieve_messge", data);
-    });
+        //find room
+        let room;
+        for(let i = 0; i < rooms.length; i++){
+            if(rooms[i].id === data.room){
+                room = rooms[i];
+            }
+        }
+        room.chat.push({name: data.name, message: data.message});
+        
+        io.to(data.room).emit("receive_message", room.chat);
+    })
 });
 
 server.listen(3001, () => {
